f0e74a0e3c0b1ec8c76cb8b094d7488ee62191a8
Samuel Calderon <samuel.calderon@uarm.pe>
2023-09-26 14:11:21 -0500

    add manual delay between requests

--- R/infomidis.R
+++ R/infomidis.R
@@ -3,7 +3,7 @@
 #' @param ambito Ámbito de la consulta. Uno de c("nacional", "vraem")
 #' @param periodo_anual,mes Año y mes de los datos de consulta. Año debe especificarse en formato "YYYY" y mes en formato "MM". Se tienen datos de Octubre 2012.
 #' @param departamento,provincia,distrito Código de lugar de búsqueda. Cada uno debe especificarse en formato "00". Solo necesario si se requiere mayor nivel de detalle.
-#' 
+#' @param sleep_time Time to wait between requests. Only useful when iterating over multiple calls to this function.
 #' @return Un `<tibble>` con la información consultada
 #' @export
 infomidis <- function(ambito = "nacional",
@@ -11,10 +11,10 @@ infomidis <- function(ambito = "nacional",
 					  mes = ultima_data_mes(),
 					  departamento = NULL,
 					  provincia = NULL,
-					  distrito = NULL) {
+					  distrito = NULL,
+					  sleep_time = 0L) {
 	
 	ambito <- match.arg(ambito, c("nacional", "vraem"))
-	
 	
 	im_request <- httr2::request("http://sdv.midis.gob.pe/Infomidis/api/programasSociales") %>% 
 		httr2::req_url_query(ambito = ambito) %>% 
@@ -22,6 +22,8 @@ infomidis <- function(ambito = "nacional",
 		httr2::req_url_query(depto = departamento) %>%
 		httr2::req_url_query(prov = provincia) %>% 
 		httr2::req_url_query(dist = distrito) 
+	
+	Sys.sleep(time = sleep_time)
 	
 	im_request %>% 
 		httr2::req_perform() %>% 
