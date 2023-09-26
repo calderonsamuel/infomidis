71c74bacef93e60b6e7ee799dcafafc81e8bc3dc
Samuel Calderon <samuel.calderon@uarm.pe>
2023-09-26 12:40:30 -0500

    export infomidis

---
+++ R/infomidis.R
@@ -0,0 +1,35 @@
+#' Descargar datos de InfoMIDIS
+#' 
+#' @param ambito Ámbito de la consulta. Uno de c("nacional", "vraem")
+#' @param periodo_anual,mes Año y mes de los datos de consulta. Año debe especificarse en formato "YYYY" y mes en formato "MM". Se tienen datos de Octubre 2012.
+#' @param departamento,provincia,distrito Código de lugar de búsqueda. Cada uno debe especificarse en formato "00". Solo necesario si se requiere mayor nivel de detalle.
+#' 
+#' @return Un `<tibble>` con la información consultada
+#' @export
+infomidis <- function(ambito = "nacional",
+					  periodo_anual = ultima_data_anio(),
+					  mes = ultima_data_mes(),
+					  departamento = NULL,
+					  provincia = NULL,
+					  distrito = NULL) {
+	
+	ambito <- match.arg(ambito, c("nacional", "vraem"))
+	
+	
+	im_request <- httr2::request("http://sdv.midis.gob.pe/Infomidis/api/programasSociales") %>% 
+		httr2::req_url_query(ambito = ambito) %>% 
+		httr2::req_url_query(periodo = glue::glue("{periodo_anual}{mes}")) %>% 
+		httr2::req_url_query(depto = departamento) %>%
+		httr2::req_url_query(prov = provincia) %>% 
+		httr2::req_url_query(dist = distrito) 
+	
+	im_request %>% 
+		httr2::req_perform() %>% 
+		httr2::resp_body_json() %>% 
+		purrr::map(tibble::as_tibble) %>% 
+		purrr::list_rbind()
+}
+
+ultima_data_anio <- function() (Sys.Date() - lubridate::period(1, "month")) %>% format("%Y") 
+
+ultima_data_mes <- function() (Sys.Date() - lubridate::period(1, "month")) %>% format("%m") 
